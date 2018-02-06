9627cfaf72c05b4465bd7f1294d19909e626f8cc
Jared Ross <JaredARoss14@gmail.com>
2018-02-06 16:50:53 -0500

    setting up heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+// Set up express app
+var app = express();
+var PORT = process.env.PORT || 8080
+
+app.use(express.static(__dirname));
+
+// Routes
+
+app.get("/", function (req, res) {
+  res.sendfile("app/public/home.html")
+})
+
+app.listen(PORT, function () {
+  console.log("app running");
+})
