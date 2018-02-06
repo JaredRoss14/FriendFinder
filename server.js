a26af4f2f6b7f01c76d04841663f8c43ae0f3468
Jared Ross <JaredARoss14@gmail.com>
2018-02-06 16:58:02 -0500

    Fixed app.use directory to public

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/app/public"));
 
 // Routes
 
